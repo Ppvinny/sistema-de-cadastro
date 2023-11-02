d66fcda5b0c6ffdfb4cf9c1f6f39e4dae863487f
vinicius <vinnyseries@hotmail.com>
2023-11-01 21:21:55 -0300

    corrigindo o console app js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 
-console.log("teste para Git add, atualizar a versao desktop")
+console.log("teste para Git add, atualizar a versão desktop")
